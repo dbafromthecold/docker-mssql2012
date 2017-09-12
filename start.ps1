ffd4111003286b8042f7c0759f5fe9d19f380fb2
Andrew Pruski <dbafromthecold@gmail.com>
2017-09-12 17:09:13 +0100

    Add files via upload

---
+++ start.ps1
@@ -0,0 +1,38 @@
+param(
+[Parameter(Mandatory=$false)]
+[string]$sa_password,
+
+[Parameter(Mandatory=$false)]
+[string]$ACCEPT_EULA
+)
+
+
+if($ACCEPT_EULA -ne "Y" -And $ACCEPT_EULA -ne "y")
+{
+	Write-Verbose "ERROR: You must accept the End User License Agreement before this container can start."
+	Write-Verbose "Set the environment variable ACCEPT_EULA to 'Y' if you accept the agreement."
+
+    exit 1
+}
+
+# start the service
+Write-Verbose "Starting SQL Server"
+start-service MSSQLSERVER
+
+if($sa_password -ne "_")
+{
+    Write-Verbose "Changing SA login credentials"
+    $sqlcmd = "ALTER LOGIN sa with password=" +"'" + $sa_password + "'" + ";ALTER LOGIN sa ENABLE;"
+    & sqlcmd -Q $sqlcmd
+}
+
+
+Write-Verbose "Started SQL Server."
+
+$lastCheck = (Get-Date).AddSeconds(-2) 
+while ($true) 
+{ 
+    Get-EventLog -LogName Application -Source "MSSQL*" -After $lastCheck | Select-Object TimeGenerated, EntryType, Message	 
+    $lastCheck = Get-Date 
+    Start-Sleep -Seconds 2 
+}
